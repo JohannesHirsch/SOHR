1b2e8fd1d176b4806ccc0f359fc9edc7d749d7cd
JohannesHirsch <johannes.hirsch2@students.fh-wels.at>
2017-01-02 12:22:34 +0100

    Zwischenstand

---
+++ SOHR/SOHR.Shared/Classes/ModelBase.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOHR.Shared
+{
+    public class ModelBase
+    {
+        #region FIELDS
+        #endregion // FIELDS
+
+        #region CONSTRUCTOR
+        #endregion // CONSTRUCTOR
+
+        #region PROPERTIES
+        /// <summary>
+        /// Id des Satzed von Fragen
+        /// </summary>
+        public Guid ID { get; set; }
+        /// <summary>
+        /// Name des Satzes von Fragen
+        /// </summary>
+        public string Name { get; set; }        
+
+        #endregion // PROPERTIES
+
+        #region PRIVATE METHODS
+        #endregion PRIVATE METHODS
+
+        #region PUBLIC METHODS
+        #endregion // PUBLIC METHODS
+    }
+}
